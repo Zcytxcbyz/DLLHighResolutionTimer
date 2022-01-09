c2dc49532bfa73a31bab8e2f55a129a6fa810a8d
kyrox02 <wfqwqfwqf@gmail.com>
2022-01-09 16:29:09 +0200

    Update GetCycleCount64.asm
    final fix for loops in RDTSC take me days to figure this out

--- source/GetCycleCount64.asm
+++ source/GetCycleCount64.asm
@@ -1,10 +1,8 @@
 .code
-  GetCycleCount proc 
-    push rdx
-	rdtsc
-	shl rdx, 32
-	or  rax, rdx
-	pop rdx
-	ret
+  GetCycleCount proc
+    cpuid
+    rdtsc
+    cpuid
+    ret
   GetCycleCount endp
 end
